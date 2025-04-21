f3c3c7078c07bae1767163272b64c8029b94cd88
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-04-21 16:33:23 -0600

    Mejoras de código en el reto

--- primerprograma/src/DesafioCajero.java
+++ primerprograma/src/DesafioCajero.java
@@ -5,97 +5,54 @@ public class DesafioCajero {
         double dinero = 1599.99;
         Scanner scanner = new Scanner(System.in);
         int selector;
-        double retiroDeposito = 0;
+        double retiroDeposito;
 
-        String menuPrincipal = String.format("""
-                ***********************************************
-                
-                Nombre del cliente: Tony Stark
-                Tipo de cuenta: Corriente
-                Saldo disponible: %.2f $
-                
-                ***********************************************
+        System.out.println("***********************************************\n");
+        System.out.println("Nombre del cliente: Tony Stark");
+        System.out.println("Tipo de cuenta: Corriente");
+        System.out.printf("Saldo disponible: %.2f $\n\n", dinero);
+        System.out.println("***********************************************");
+
+        String menuPrincipal = """
                 *** Escriba el número de la opción que desea ***
                 1.- Consultar saldo
                 2.- Retirar efectivo
                 3.- Depositar
                 9.- Salir
-                """, dinero);
+                """;
 
         System.out.println(menuPrincipal);
 
         selector = scanner.nextInt();
         while(selector!=9){
             if(selector == 1){
-                menuPrincipal = String.format("""
-                        Su saldo es: %.2f $
-                        *** Escriba el número de la opción que desea ***
-                        1.- Consultar saldo
-                        2.- Retirar efectivo
-                        3.- Depositar
-                        9.- Salir
-                        """, dinero);
+                System.out.printf("Saldo disponible: %.2f $\n",dinero);
                 System.out.println(menuPrincipal);
             }
             else if (selector == 2) {
                 System.out.println("¿Cual es la cantidad que deseas retirar?");
                 retiroDeposito = scanner.nextDouble();
                 if(retiroDeposito>dinero){
-                    menuPrincipal =
-                            """
-                            El saldo es insuficiente
-                            *** Escriba el número de la opción que desea ***
-                            1.- Consultar saldo
-                            2.- Retirar efectivo
-                            3.- Depositar
-                            9.- Salir
-                            """;
+                    System.out.println("El saldo es insuficiente");
                     System.out.println(menuPrincipal);
                 }
                 else {
-                    dinero -= retiroDeposito;
-                    menuPrincipal =
-                            String.format("""
-                            El saldo restante es: %.2f $
-                            *** Escriba el número de la opción que desea ***
-                            1.- Consultar saldo
-                            2.- Retirar efectivo
-                            3.- Depositar
-                            9.- Salir
-                            """, dinero);
+                    System.out.printf("El saldo restante es: %.2f $%n", dinero -= retiroDeposito);
                     System.out.println(menuPrincipal);
                 }
             }
             else if (selector == 3) {
                 System.out.println("¿Cuál es el valor que vas a depositar?");
                 retiroDeposito = scanner.nextDouble();
-                dinero += retiroDeposito;
-                menuPrincipal = String.format(
-                        """
-                        El saldo actualizado es: %.2f $
-                        *** Escriba el número de la opción que desea ***
-                        1.- Consultar saldo
-                        2.- Retirar efectivo
-                        3.- Depositar
-                        9.- Salir
-                        """, dinero);
+                System.out.printf("El saldo actualizado es: %.2f $\n",dinero += retiroDeposito);
                 System.out.println(menuPrincipal);
             }
             else {
-                menuPrincipal =
-                        """
-                        Esa opción no es válida, por favor ingrese una opción válida del menú
-                        *** Escriba el número de la opción que desea ***
-                        1.- Consultar saldo
-                        2.- Retirar efectivo
-                        3.- Depositar
-                        9.- Salir
-                        """;
+                System.out.println("Esa opción no es válida, por favor ingrese una opción válida del menú");
                 System.out.println(menuPrincipal);
             }
             selector = scanner.nextInt();
         }
         System.out.println("Finalizando el programa. Muchas gracias por usar nuestros servicios");
-
     }
 }
